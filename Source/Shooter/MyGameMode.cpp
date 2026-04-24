af4c2374415fe27777cd19dbbdff1616869fcce4
miltonnm123 <miltonnm123@hotmail.com>
2026-04-24 17:10:33 +0200

    spawning rework
    Spawning is not handled in gamemode but i noticed i need to have an actual object in the level so i will make an actor that just holds spawnpoints, need reference for it in gamemode.cpp to work

--- Source/Shooter/MyGameMode.cpp
+++ Source/Shooter/MyGameMode.cpp
@@ -1,5 +1,6 @@
 #include "MyGameMode.h"
 #include "MyCharacter.h"
+#include "MyGameInstance.h"
 #include "Kismet/KismetMathLibrary.h"
 #include "GameFramework/PlayerStart.h"
 
@@ -11,6 +12,13 @@ AMyGameMode::AMyGameMode()
 void AMyGameMode::BeginPlay()
 {
 	Super::BeginPlay();
+
+	UMyGameInstance* GI = Cast<UMyGameInstance>(GetGameInstance());
+	if (GI) CurrentDifficulty = GI->SelectedDifficulty;
+
+	UE_LOG(LogTemp, Warning, TEXT("Selected Difficulty: %d"), GI->SelectedDifficulty);
+
+	SpawnAllEnemies(CurrentDifficulty);
 
 
 	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AMyEnemy::StaticClass(), Enemies);
@@ -71,5 +79,58 @@ void AMyGameMode::ResetAll()
 {
 	ResetPlayer();
 	ResetEnemies();
+}
+
+void AMyGameMode::SpawnAllEnemies(int32 Difficulty)
+{
+	if (!EnemyClass)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("EnemySpawner: No EnemyClass set"));
+		return;
+	}
+
+	if (Difficulty == HardDifficulty)
+	{
+		SpawnCount *= 2;
+	}
+
+	if (Difficulty == MediumDifficulty)
+	{
+		SpawnCount *= 1.5f;
+	}
+
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+
+	TArray<AActor*> AvailablePoints = SpawnPoints;
+	for (int32 i = AvailablePoints.Num() - 1; i > 0; i--)
+	{
+		int32 j = FMath::RandRange(0, i);
+		AvailablePoints.Swap(i, j);
+	}
+
+	int32 ActualSpawnCount = FMath::Min(SpawnCount, AvailablePoints.Num());
+
+	for (int32 i = 0; i < ActualSpawnCount; i++)
+	{
+		AActor* SpawnPoint = AvailablePoints[i];
+
+		if (!IsValid(SpawnPoint)) continue;
+
+		FVector Location = SpawnPoint->GetActorLocation();
+		FRotator Rotation = SpawnPoint->GetActorRotation();
+
+		AActor* SpawnedEnemy = GetWorld()->SpawnActor<AActor>(EnemyClass, Location, Rotation, SpawnParams);
+
+		if (SpawnedEnemy)
+		{
+			UE_LOG(LogTemp, Log, TEXT("Spawned enemy at %s"), *Location.ToString());
+		}
+	}
+}
+
+void AMyGameMode::SetCurrentDifficulty(int NewValue)
+{
+	CurrentDifficulty = NewValue;
 }
 
--- Source/Shooter/MyGameMode.h
+++ Source/Shooter/MyGameMode.h
@@ -2,6 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "GameFramework/Actor.h"
 #include "Kismet/GameplayStatics.h"
 #include "MyEnemy.h"
 #include "MyGameMode.generated.h"
@@ -21,6 +22,19 @@ public:
 	void ResetPlayer();
 	void ResetAll();
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning")
+	int32 SpawnCount = 3;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning")
+	int32 CurrentDifficulty = 2;
+
+	int32 EasyDifficulty = 1;
+	int32 MediumDifficulty = 2;
+	int32 HardDifficulty = 3;
+
+	UFUNCTION(BlueprintCallable, Category = "Spawning")
+	void SetCurrentDifficulty(int NewValue);
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	TArray<AActor*> Enemies;
 
@@ -29,4 +43,12 @@ protected:
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
 	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning")
+	TSubclassOf<AActor> EnemyClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning")
+	TArray<AActor*> SpawnPoints;
+
+	UFUNCTION(BlueprintCallable, Category = "Spawning")
+	void SpawnAllEnemies(int32 Difficulty);
 };
